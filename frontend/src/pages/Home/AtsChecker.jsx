f7b4b507aa0082b404f4905ddc725ba71ffeedd8
khursed1 <khursednew@gmail.com>
2025-05-12 19:20:52 +0530

    .

--- frontend/src/pages/Home/AtsChecker.jsx
+++ frontend/src/pages/Home/AtsChecker.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Navbar from '../../components/layouts/Navbar'
 
 const AtsChecker = () => {
   return (
+    <>
+    <Navbar/>
     <div>AtsChecker</div>
+    </>
+    
   )
 }
 
